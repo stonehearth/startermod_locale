c1a54f70ea5c6f92ba77c9bb3589c1c1fda1ea86
Benjamin <benjamin.egon@fwa.eu>
2015-10-07 15:38:19 +0200

    forget to set UTF8, hack

---
+++ fr-README.cs
@@ -0,0 +1,8 @@
+//Consignes :
+//
+//Ne pas oublier qu'en Français il faut un espace devant les caractères de
+//ponctuations suivant ;:!?
+//
+//En Français les majuscules ce n'est que : en début de phrase, et pour les nom propres.
+//
+//Encodage en UTF8 (sans bom)
--- fr-README.txt
+++
@@ -1,6 +0,0 @@
-Consignes :
-
-Ne pas oublier qu'en Français il faut un espace devant les caractères de
-ponctuations suivant ;:!?
-
-En Français les majuscules ce n'est que : en début de phrase, et pour les nom propres.
